f6be181a78cc7a8e2b1ab3b846bcedc567a781d6
CAZALAC <pablo.ignacio.rt@gmail.com>
2025-08-18 01:44:29 -0400

    Mejora la lógica de inicio de sesión de usuario al permitir la resolución de nombres de usuario a partir de correos electrónicos. Se añade registro detallado para facilitar la depuración y se mejora el manejo de errores en el proceso de autenticación.

--- ckanext/auth/logic.py
+++ ckanext/auth/logic.py
@@ -5,6 +5,9 @@ from ckan.common import _
 
 _check_access = logic.check_access
 
+log = tk.get_logger(__name__)
+
+
 def user_login(context, data_dict):
     # Adapted from  https://github.com/ckan/ckan/blob/master/ckan/views/user.py#L203-L211
     generic_error_message = {
@@ -15,24 +18,43 @@ def user_login(context, data_dict):
     }
 
     # Accept either username or email in the incoming identifier
-    login_value = data_dict.get('id') or data_dict.get('login')
+    login_value = (data_dict.get('id') or data_dict.get('login') or u'').strip()
     password_value = data_dict.get('password')
 
     if not login_value or not password_value:
         return generic_error_message
 
-    identity = {
-        u'login': login_value,
-        u'password': password_value
-    }
+    # Resolve to username if an email was provided
+    model = context['model']
+    resolved_username = None
+    try:
+        if '@' in login_value:
+            log.debug('user_login: identifier looks like an email, resolving to username')
+            user_from_email = (
+                model.Session.query(model.User)
+                .filter(model.User.email.ilike(login_value))
+                .first()
+            )
+            if user_from_email:
+                resolved_username = user_from_email.name
+                log.debug('user_login: resolved email to username=%s', resolved_username)
+        else:
+            # Might already be a username
+            resolved_username = login_value
+    except Exception as exc:
+        # Do not leak sensitive details; just log for admins
+        log.warning('user_login: exception while resolving identifier to username: %r', exc)
+        resolved_username = login_value
+
+    identity = {u'login': resolved_username or login_value, u'password': password_value}
 
     auth = authenticator.UsernamePasswordAuthenticator()
     auth_user_name = auth.authenticate(context, identity)
 
     if not auth_user_name:
+        log.info('user_login: authentication failed for identifier="%s" (resolved_username="%s")', login_value, resolved_username)
         return generic_error_message
 
-    model = context['model']
     user_obj = model.User.get(auth_user_name)
     if not user_obj:
         return generic_error_message
